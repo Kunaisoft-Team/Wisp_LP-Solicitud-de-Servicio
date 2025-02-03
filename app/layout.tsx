677cc5d5a2ba968f2bc2d75020735374fc4c14b1
CEO Kunaisoft <projects@kunaisoft.com>
2025-02-03 18:12:38 -0400

    update: change the page title and page description.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Wisp Tachira | Solicitud de Servicio',
+  description: 'Internet Rápido y Confiable para Táchira',
 }
 
 export default function RootLayout({
